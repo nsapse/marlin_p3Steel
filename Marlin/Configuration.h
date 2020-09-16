1d41f35f34a3da5d063715f92d89a4f7001d1814
nsapse <nsapse@gmail.com>
2020-09-16 15:38:11 -0400

    final pid tune

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -487,9 +487,10 @@
   // If you are using a pre-configured hotend then you can use one of the value sets by uncommenting it
 
   // Ultimaker
-  #define DEFAULT_Kp 21.89
-  #define DEFAULT_Ki 7.25
-  #define DEFAULT_Kd 68.16
+  
+#define DEFAULT_Kp 30.68
+#define DEFAULT_Ki 2.39
+#define DEFAULT_Kd 98.66
 
   // MakerGear
   //#define DEFAULT_Kp 7.0
