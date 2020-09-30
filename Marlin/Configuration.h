64f8e3260623fd18b6dfd13788c1a43dd050db75
nsapse <nsapse@gmail.com>
2020-09-30 15:56:49 -0400

    Tuned bed PID

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -521,7 +521,7 @@
  * heater. If your configuration is significantly different than this and you don't understand
  * the issues involved, don't use bed PID until someone else verifies that your hardware works.
  */
-//#define PIDTEMPBED
+#define PIDTEMPBED
 
 //#define BED_LIMIT_SWITCHING
 
@@ -539,9 +539,9 @@
 
   //120V 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
   //from FOPDT model - kp=.39 Tp=405 Tdead=66, Tc set to 79.2, aggressive factor of .15 (vs .1, 1, 10)
-  #define DEFAULT_bedKp 10.00
-  #define DEFAULT_bedKi .023
-  #define DEFAULT_bedKd 305.4
+  #define DEFAULT_bedKp 69.90
+  #define DEFAULT_bedKi 5.30
+  #define DEFAULT_bedKd 614.58
 
   //120V 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
   //from pidautotune
@@ -734,7 +734,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 152}
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 950}
 
 /**
  * Default Max Feed Rate (mm/s)
